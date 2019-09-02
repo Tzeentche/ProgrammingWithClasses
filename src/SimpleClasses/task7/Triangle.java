2f3544dd8aa77e8788554db17a992b2ccae8d462
Tzeentche <ilyasugako87@gmail.com>
2019-09-02 23:39:14 +0300

    Started developing decision for task 4.1.7 "Create class for calculate triangle's parameters" (EPAM, 4.Programming with classes)

---
+++ src/SimpleClasses/task7/Triangle.java
@@ -0,0 +1,12 @@
+package SimpleClasses.task7;
+
+//      Описать класс, представляющий треугольник. Предусмотреть методы для создания объектов, вычисления площади,
+//      периметра и точки пересечения медиан.
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+
+    }
+}
