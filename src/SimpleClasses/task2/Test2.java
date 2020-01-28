0a55f2007cf1767d3fdb49269ee6a3620f1cadf5
Tzeentche <ilyasugako87@gmail.com>
2020-01-28 23:00:16 +0300

    Crate code to "Test2" for task 4.1.2 (EPAM, 4.Programming with classes)

--- src/SimpleClasses/task2/Test2.java
+++ src/SimpleClasses/task2/Test2.java
@@ -3,9 +3,18 @@ package SimpleClasses.task2;
 //        Создйте класс Test2 двумя переменными. Добавьте конструктор с входными параметрами. Добавьте конструктор,
 //        инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра класса.
 
-public class TwoVariables {
+public class Test2 {
 
-    public static void main(String[] args) {
+    int firstVariable;
+    int secondVariable;
 
+    public void test2() {
+        
+    }
+
+    public void test2() {
+
+        firstVariable = 0;
+        secondVariable = 0;
     }
 }
