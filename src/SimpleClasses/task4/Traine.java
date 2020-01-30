2f8631f990682d509b357b6c79537c17fc3365a1
Tzeentche <ilyasugako87@gmail.com>
2020-01-30 23:41:41 +0300

    Create class with methods for task 4.1.4 (EPAM, 4.Programming with classes)

---
+++ src/SimpleClasses/task4/Traine.java
@@ -0,0 +1,26 @@
+package SimpleClasses.task4;
+
+public class Traine {
+
+    String destinationPoint;
+    int trainNumber;
+    String timeToGo;
+
+    public int numbersOfTrain(int numbers) {
+
+    return 0;
+    }
+
+    public String usersInput(String inputTrain) {
+
+        String value = "s";
+        return value;
+    }
+
+    public String destinationPointSorting(String destination) {
+
+        String destPoint = "l";
+
+        return destPoint;
+    }
+}
