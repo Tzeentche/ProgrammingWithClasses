8c3aa121f8d94c87ca4726e3bbafd4362b03f7ad
Tzeentche <ilyasugako87@gmail.com>
2020-02-01 22:42:50 +0300

    Refactored code in class Studentt for task 4.1.3 (EPAM, 4.Programming with classes)

--- src/SimpleClasses/task3/Studentt.java
+++ src/SimpleClasses/task3/Studentt.java
@@ -1,8 +1,20 @@
 package SimpleClasses.task3;
 
+//        Создайте класс с именем Student, содержащий поля: фамилия и инициалы, номер группы, успеваемость
+//        (массив из пяти элементов). Создайте массив из десяти элементов такого типа.
+//        Добавьте возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 9 или 10.
+
 public class Studentt {
 
-    String lastName;
+    String name;
     int classNumber;
     int[] achives = new int[5];
+    double medium;
+
+    public Studentt(String name,int classNumber, int[] achives) {
+
+        this.name = name;
+        this.classNumber =classNumber;
+        this.achives = achives;
+    }
 }
