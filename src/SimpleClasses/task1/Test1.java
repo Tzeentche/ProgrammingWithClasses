344a678623fe569ba0aaacd41c639faed6847c9b
Tzeentche <ilyasugako87@gmail.com>
2020-02-02 21:55:16 +0300

    Add comments to task 4.1.1 (EPAM, 4.Programming with classes)

--- src/SimpleClasses/task1/Test1.java
+++ src/SimpleClasses/task1/Test1.java
@@ -2,13 +2,19 @@ package SimpleClasses.task1;
 
 public class Test1 {
 
+//    Declare global variables:
+
     int first;
     int second;
+
+//      Output method:
 
     public void output() {
 
         System.out.println("First value is " + first + ", second value is " + second);
     }
+
+//    Correct methods:
 
     public void setFirst(int first) {
         this.first = first;
@@ -26,11 +32,15 @@ public class Test1 {
         return second;
     }
 
+//    Summ finder method:
+
     public int sum(int valueOne, int valueTwo) {
 
         return valueOne + valueTwo;
     }
 
+//    Max value finder method:
+    
     public int maxValue(int valueOne, int valueTwo) {
 
         int max = valueOne;
