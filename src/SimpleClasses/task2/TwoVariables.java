ba99d47258fa8fa961f6234218115b544b6b00ac
Tzeentche <ilyasugako87@gmail.com>
2020-02-04 23:31:55 +0300

    Add changes to task 4.1.2 (EPAM, 4.Programming with classes)

--- src/SimpleClasses/task2/TwoVariables.java
+++ src/SimpleClasses/task2/TwoVariables.java
@@ -8,7 +8,7 @@ public class TwoVariables {
     public static void main(String[] args) {
 
 //        Create class example:
-        
+
         Test2 test2 = new Test2(2, 4);
         System.out.println("First variable is " + test2.firstVariable + ", second variable is " + test2.secondVariable);
     }
